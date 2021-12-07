71b0be982d414abe68409b4dbd153d58aba0ec49
DerToaster98 <seiler.oli@web.de>
2021-12-07 19:55:51 +0100

    implement different charging times

--- src/main/java/de/dertoaster/crossbowverhaul/item/ItemCrossbow.java
+++ src/main/java/de/dertoaster/crossbowverhaul/item/ItemCrossbow.java
@@ -3,9 +3,8 @@ package de.dertoaster.crossbowverhaul.item;
 import java.util.List;
 import java.util.function.Predicate;
 
-import com.google.common.collect.Lists;
-
 import de.dertoaster.crossbowverhaul.init.ModItems;
+import de.dertoaster.crossbowverhaul.mixin.CrossbowItemInvoker;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.enchantment.IVanishable;
@@ -15,13 +14,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArrowItem;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.world.World;
 
-public class ItemCrossbow extends CrossbowItem implements IVanishable {
+public class ItemCrossbow extends CrossbowItem implements IVanishable, IModifiedCrossbowMethod {
 
 	protected static final Predicate<ItemStack> PREDICATE_BOLTS_ONLY = (itemstack) -> {
 		return itemstack.getItem() instanceof ItemBolt && !(itemstack.getItem() instanceof ItemBoltExplosive);
@@ -49,47 +46,14 @@ public class ItemCrossbow extends CrossbowItem implements IVanishable {
 	//TODO: Move CrossbowItem.shootProjectile() to Item
 	
 	@Override
-	public int getUseDuration(ItemStack item) {
-		// As wished by brass: 50% SLOWER loading time => x2
-		return 2 * super.getUseDuration(item);
-	}
-	
-	// Copied from vanilla cause for fucks sake methods are static -___-
-
-	@Override
 	public void releaseUsing(ItemStack weaponItem, World world, LivingEntity shooter, int useDuration) {
 		int i = this.getUseDuration(weaponItem) - useDuration;
-		float f = this.getPowerForTime(i, weaponItem);
+		float f = CrossbowItem.getPowerForTime(i, weaponItem);
 		if (f >= 1.0F && !CrossbowItem.isCharged(weaponItem) && this.tryLoadProjectiles(shooter, weaponItem)) {
 			CrossbowItem.setCharged(weaponItem, true);
 			SoundCategory soundcategory = shooter instanceof PlayerEntity ? SoundCategory.PLAYERS : SoundCategory.HOSTILE;
 			world.playSound((PlayerEntity) null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.CROSSBOW_LOADING_END, soundcategory, 1.0F, 1.0F / (random.nextFloat() * 0.5F + 1.0F) + 0.2F);
 		}
-	}
-
-	// More vanilla copies...
-	protected float getPowerForTime(int useDuration, ItemStack weaponItem) {
-		float f = (float) useDuration / (float) getChargeDuration(weaponItem);
-		if (f > 1.0F) {
-			f = 1.0F;
-		}
-
-		return f;
-	}
-
-	protected void addChargedProjectile(ItemStack weaponItem, ItemStack projectileItem) {
-		CompoundNBT compoundnbt = weaponItem.getOrCreateTag();
-		ListNBT listnbt;
-		if (compoundnbt.contains("ChargedProjectiles", 9)) {
-			listnbt = compoundnbt.getList("ChargedProjectiles", 10);
-		} else {
-			listnbt = new ListNBT();
-		}
-
-		CompoundNBT compoundnbt1 = new CompoundNBT();
-		projectileItem.save(compoundnbt1);
-		listnbt.add(compoundnbt1);
-		compoundnbt.put("ChargedProjectiles", listnbt);
 	}
 
 	// Attention: this actually differs from vanilla!
@@ -110,7 +74,7 @@ public class ItemCrossbow extends CrossbowItem implements IVanishable {
 				itemstack1 = itemstack.copy();
 			}
 
-			if (!loadProjectile(shooter, weaponItem, itemstack, k > 0, flag)) {
+			if (!this.loadProjectile(shooter, weaponItem, itemstack, k > 0, flag)) {
 				return false;
 			}
 		}
@@ -140,28 +104,11 @@ public class ItemCrossbow extends CrossbowItem implements IVanishable {
 				itemstack = projectileItem.copy();
 			}
 
-			addChargedProjectile(weaponItem, itemstack);
+			CrossbowItemInvoker.invokeAddChargedProjectile(weaponItem, itemstack);
 			return true;
 		}
 	}
 	
-	//Copy from vanilla
-	public static List<ItemStack> getChargedProjectiles(ItemStack p_220018_0_) {
-	      List<ItemStack> list = Lists.newArrayList();
-	      CompoundNBT compoundnbt = p_220018_0_.getTag();
-	      if (compoundnbt != null && compoundnbt.contains("ChargedProjectiles", 9)) {
-	         ListNBT listnbt = compoundnbt.getList("ChargedProjectiles", 10);
-	         if (listnbt != null) {
-	            for(int i = 0; i < listnbt.size(); ++i) {
-	               CompoundNBT compoundnbt1 = listnbt.getCompound(i);
-	               list.add(ItemStack.of(compoundnbt1));
-	            }
-	         }
-	      }
-
-	      return list;
-	   }
-
 	public static int getBoltTier(ItemStack itemStack) {
 		List<ItemStack> loadedProjectiles = getChargedProjectiles(itemStack);
 		if(loadedProjectiles != null && !loadedProjectiles.isEmpty()) {
@@ -174,5 +121,11 @@ public class ItemCrossbow extends CrossbowItem implements IVanishable {
 		}
 		return 0;
 	}
-
+	
+	//Override charging duration
+	@Override
+	public int getMaxChargeTime() {
+		return 2 * IModifiedCrossbowMethod.super.getMaxChargeTime();
+	}
+	
 }
--- src/main/java/de/dertoaster/crossbowverhaul/item/ItemCrossbowNetherite.java
+++ src/main/java/de/dertoaster/crossbowverhaul/item/ItemCrossbowNetherite.java
@@ -5,16 +5,16 @@ import java.util.function.Predicate;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
-public class ItemCrossbowNetherite extends ItemCrossbow {
-	
+public class ItemCrossbowNetherite extends ItemCrossbow implements IModifiedCrossbowMethod {
+
 	protected static final Predicate<ItemStack> PREDICATE_BOLTS = (itemstack) -> {
 		return itemstack.getItem() instanceof ItemBolt;
 	};
-	
+
 	protected static final Predicate<ItemStack> PREDICATE_BOLTS_OR_FIREWORK = PREDICATE_BOLTS.or((itemStack) -> {
 		return itemStack.getItem() == Items.FIREWORK_ROCKET;
 	});
-	
+
 	@Override
 	public Predicate<ItemStack> getSupportedHeldProjectiles() {
 		return PREDICATE_BOLTS_OR_FIREWORK;
@@ -28,7 +28,7 @@ public class ItemCrossbowNetherite extends ItemCrossbow {
 	public ItemCrossbowNetherite(Properties properties) {
 		super(properties);
 	}
-	
+
 	@Override
 	public int getUseDuration(ItemStack item) {
 		return 2 * super.getUseDuration(item);
@@ -38,5 +38,11 @@ public class ItemCrossbowNetherite extends ItemCrossbow {
 	public int getDefaultProjectileRange() {
 		return 2 * super.getDefaultProjectileRange();
 	}
-	
+
+	// Override charging duration
+	@Override
+	public int getMaxChargeTime() {
+		return 2 * super.getMaxChargeTime();
+	}
+
 }
