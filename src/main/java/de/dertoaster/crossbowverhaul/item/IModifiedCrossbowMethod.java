40d890da9949621b34f4130a1cff258aab4aad53
DerToaster98 <seiler.oli@web.de>
2021-12-07 19:53:37 +0100

    haha mixin go brrrrrrrr

---
+++ src/main/java/de/dertoaster/crossbowverhaul/item/IModifiedCrossbowMethod.java
@@ -0,0 +1,93 @@
+package de.dertoaster.crossbowverhaul.item;
+
+import java.util.List;
+
+import de.dertoaster.crossbowverhaul.mixin.CrossbowItemInvoker;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.ICrossbowUser;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.FireworkRocketEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.vector.Quaternion;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.world.World;
+
+public interface IModifiedCrossbowMethod {
+	
+	public default void modifiedPerformShooting(World world, LivingEntity shooter, Hand handUsed, ItemStack crossbow, float speed, float divergence) {
+		 List<ItemStack> list = CrossbowItem.getChargedProjectiles(crossbow);
+	      float[] afloat = CrossbowItem.getShotPitches(shooter.getRandom());
+
+	      for(int i = 0; i < list.size(); ++i) {
+	         ItemStack itemstack = list.get(i);
+	         boolean flag = shooter instanceof PlayerEntity && ((PlayerEntity)shooter).abilities.instabuild;
+	         if (!itemstack.isEmpty()) {
+	            if (i == 0) {
+	            	CrossbowItemInvoker.invokeShootProjectile(world, shooter, handUsed, crossbow, itemstack, afloat[i], flag, speed, divergence, 0.0F);
+	            } else if (i == 1) {
+	            	CrossbowItemInvoker.invokeShootProjectile(world, shooter, handUsed, crossbow, itemstack, afloat[i], flag, speed, divergence, -10.0F);
+	            } else if (i == 2) {
+	            	CrossbowItemInvoker.invokeShootProjectile(world, shooter, handUsed, crossbow, itemstack, afloat[i], flag, speed, divergence, 10.0F);
+	            }
+	         }
+	      }
+
+	      CrossbowItemInvoker.invokeOnCrossbowShot(world, shooter, crossbow);
+	}
+	
+	public default void modifiedShootProjectile(World world, LivingEntity shooter, Hand handUsed, ItemStack crossbow, ItemStack projectileStack, float shootSoundPitch, boolean flagProjectileCantBePickedUp, float speed, float divergence, float simulated) {
+		 if (!world.isClientSide) {
+	         boolean flag = projectileStack.getItem() == Items.FIREWORK_ROCKET;
+	         ProjectileEntity projectileentity;
+	         if (flag) {
+	            projectileentity = new FireworkRocketEntity(world, projectileStack, shooter, shooter.getX(), shooter.getEyeY() - (double)0.15F, shooter.getZ(), true);
+	         } else {
+	            projectileentity = CrossbowItem.getArrow(world, shooter, crossbow, projectileStack);
+	            if (flagProjectileCantBePickedUp || simulated != 0.0F) {
+	               ((AbstractArrowEntity)projectileentity).pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
+	            }
+	         }
+
+	         if (shooter instanceof ICrossbowUser) {
+	            ICrossbowUser icrossbowuser = (ICrossbowUser)shooter;
+	            icrossbowuser.shootCrossbowProjectile(icrossbowuser.getTarget(), crossbow, projectileentity, simulated);
+	         } else {
+	            Vector3d vector3d1 = shooter.getUpVector(1.0F);
+	            Quaternion quaternion = new Quaternion(new Vector3f(vector3d1), simulated, true);
+	            Vector3d vector3d = shooter.getViewVector(1.0F);
+	            Vector3f vector3f = new Vector3f(vector3d);
+	            vector3f.transform(quaternion);
+	            projectileentity.shoot((double)vector3f.x(), (double)vector3f.y(), (double)vector3f.z(), speed, divergence);
+	         }
+
+	         crossbow.hurtAndBreak(flag ? 3 : 1, shooter, (p_220017_1_) -> {
+	            p_220017_1_.broadcastBreakEvent(handUsed);
+	         });
+	         world.addFreshEntity(projectileentity);
+	         world.playSound((PlayerEntity)null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0F, shootSoundPitch);
+	      }
+	}
+
+	public default int modifiedGetChargeDuration(ItemStack crossbow) {
+		 int i = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.QUICK_CHARGE, crossbow);
+	     return i == 0 ? this.getMaxChargeTime() : this.getMaxChargeTime() - this.getChargeTimeReductionPerQuickChargeLevel() * i;
+	}
+	
+	default int getMaxChargeTime() {
+		return 25;
+	}
+	
+	default int getChargeTimeReductionPerQuickChargeLevel() {
+		return 5;
+	}
+}
---
+++ src/main/java/de/dertoaster/crossbowverhaul/mixin/MixinCrossbowItem.java
@@ -0,0 +1,93 @@
+package de.dertoaster.crossbowverhaul.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import de.dertoaster.crossbowverhaul.item.IModifiedCrossbowMethod;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+@Mixin(CrossbowItem.class)
+public class MixinCrossbowItem {
+	
+	//Changed methods:
+	// - performShooting: Calls the method of an interface class if the item is an instance of that
+    // - getChargeDuration: Same as above
+	// - shootProjectile: Same as above
+	
+	/*
+	 * Changed the private static performShooting method to call the interface method if the item of the crossbow implements that
+	 */
+	@Inject(
+		at = @At("HEAD"), 
+		method = "performShooting(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/item/ItemStack;FF)V",
+		cancellable = true
+	)
+	private static void performShooting(
+		World world, 
+		LivingEntity shooter, 
+		Hand hand, 
+		ItemStack crossbow, 
+		float speed, 
+		float divergence, 
+		//For void types: Use CallbackInfo!!
+		CallbackInfo cir
+	) {
+		if(crossbow.getItem() instanceof IModifiedCrossbowMethod) {
+			((IModifiedCrossbowMethod)crossbow.getItem()).modifiedPerformShooting(world, shooter, hand, crossbow, speed, divergence);
+			
+			cir.cancel();
+		}
+	}
+	
+	@Inject(
+		at = @At("HEAD"),
+		method = "getChargeDuration(Lnet/minecraft/item/ItemStack;)I",
+		cancellable = true
+	)
+	private static void getChargeDuration(
+		ItemStack crossbow, 
+		CallbackInfoReturnable<Integer> cir
+	) {
+		if(crossbow.getItem() instanceof IModifiedCrossbowMethod) {
+			int result = ((IModifiedCrossbowMethod)crossbow.getItem()).modifiedGetChargeDuration(crossbow);
+			
+			cir.setReturnValue(result);
+		}
+	}
+	
+	@Inject(
+		at = @At("HEAD"),
+		method = "shoot(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;FZFFF)V",
+		cancellable = true
+	)
+	private static void shootProjectile(
+		World world, 
+		LivingEntity shooter, 
+		Hand handUsed, 
+		ItemStack crossbow, 
+		ItemStack projectileStack, 
+		float shootSoundPitch, 
+		boolean flagProjectileCantBePickedUp, 
+		float speed, 
+		float divergence, 
+		float simulated,
+		CallbackInfoReturnable<Void> cir
+	) {
+		if(crossbow.getItem() instanceof IModifiedCrossbowMethod) {
+			((IModifiedCrossbowMethod)crossbow.getItem()).modifiedShootProjectile(world, shooter, handUsed, crossbow, projectileStack, shootSoundPitch, flagProjectileCantBePickedUp, speed, divergence, simulated);
+			
+			cir.cancel();
+		}
+	}
+	
+	
+	
+	
+}
