2a19993f79499a4094d70100d267c992d7c377d2
DerToaster98 <seiler.oli@web.de>
2021-12-08 16:52:47 +0100

    format

--- src/main/java/de/dertoaster/crossbowverhaul/item/IModifiedCrossbowMethod.java
+++ src/main/java/de/dertoaster/crossbowverhaul/item/IModifiedCrossbowMethod.java
@@ -23,93 +23,94 @@ import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.world.World;
 
 public interface IModifiedCrossbowMethod {
-	
+
 	public default float getMultiShotAngle() {
-		//Default value is 10 degrees resulting in a opening angle of 20 degrees
+		// Default value is 10 degrees resulting in a opening angle of 20 degrees
 		return 22.5F;
 	}
-	
+
 	public default float[] modifiedGetShotPitches(Random rnd, final int multiShotLevel) {
-		float[] result = new float[multiShotLevel * 2 +1];
-		
-		for(int i = 0; i < multiShotLevel; i++) {
+		float[] result = new float[multiShotLevel * 2 + 1];
+
+		for (int i = 0; i < multiShotLevel; i++) {
 			boolean currentFlag = rnd.nextBoolean();
 			result[i] = CrossbowItem.getRandomShotPitch(currentFlag);
 			result[result.length - (i + 1)] = CrossbowItem.getRandomShotPitch(!currentFlag);
 		}
 		result[multiShotLevel] = 1.0F;
-		
+
 		return result;
 	}
-	
+
 	public default void modifiedPerformShooting(World world, LivingEntity shooter, Hand handUsed, ItemStack crossbow, float speed, float divergence) {
-		 List<ItemStack> list = CrossbowItem.getChargedProjectiles(crossbow);
-		 final int msLevel = (list.size() -1) / 2;
-	      	float[] afloat = this.modifiedGetShotPitches(shooter.getRandom(), msLevel);
+		List<ItemStack> list = CrossbowItem.getChargedProjectiles(crossbow);
+		final int msLevel = (list.size() - 1) / 2;
+		float[] afloat = this.modifiedGetShotPitches(shooter.getRandom(), msLevel);
 
-	      	//DONE: Adjust to not be hardcoded to indexes
-		
-		//Multishot defines additional projectiles per side
-		final float anglePerIteration = this.getMultiShotAngle() / ((float)msLevel); //Divide by the multishot level: 
+		// DONE: Adjust to not be hardcoded to indexes
+
+		// Multishot defines additional projectiles per side
+		final float anglePerIteration = this.getMultiShotAngle() / ((float) msLevel); // Divide by the multishot level:
 		float currentAngle = -this.getMultiShotAngle();
-		
-	     	for(int i = 0; i < list.size(); ++i) {
-	       		ItemStack itemstack = list.get(i);
-	         	boolean flag = shooter instanceof PlayerEntity && ((PlayerEntity)shooter).abilities.instabuild;
-	         	if (!itemstack.isEmpty()) {
+
+		for (int i = 0; i < list.size(); ++i) {
+			ItemStack itemstack = list.get(i);
+			boolean flag = shooter instanceof PlayerEntity && ((PlayerEntity) shooter).abilities.instabuild;
+			if (!itemstack.isEmpty()) {
 				CrossbowItem.shootProjectile(world, shooter, handUsed, crossbow, itemstack, afloat[i], flag, speed, divergence, currentAngle);
-	       	  	}
-	      		currentAngle += anglePerIteration;
-	      	}
-	     	CrossbowItem.onCrossbowShot(world, shooter, crossbow);
+			}
+			currentAngle += anglePerIteration;
+		}
+		CrossbowItem.onCrossbowShot(world, shooter, crossbow);
 	}
-	
-	public default void modifiedShootProjectile(World world, LivingEntity shooter, Hand handUsed, ItemStack crossbow, ItemStack projectileStack, float shootSoundPitch, boolean flagProjectileCantBePickedUp, float speed, float divergence, float simulated) {
-		 if (!world.isClientSide) {
-	         boolean flag = projectileStack.getItem() == Items.FIREWORK_ROCKET;
-	         ProjectileEntity projectileentity;
-	         if (flag) {
-	            projectileentity = new FireworkRocketEntity(world, projectileStack, shooter, shooter.getX(), shooter.getEyeY() - (double)0.15F, shooter.getZ(), true);
-	         } else {
-	            projectileentity = CrossbowItem.getArrow(world, shooter, crossbow, projectileStack);
-	            if (flagProjectileCantBePickedUp || simulated != 0.0F) {
-	               ((AbstractArrowEntity)projectileentity).pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
-	            }
-	         }
 
-	         if (shooter instanceof ICrossbowUser) {
-	            ICrossbowUser icrossbowuser = (ICrossbowUser)shooter;
-	            icrossbowuser.shootCrossbowProjectile(icrossbowuser.getTarget(), crossbow, projectileentity, simulated);
-	         } else {
-	            Vector3d vector3d1 = shooter.getUpVector(1.0F);
-	            Quaternion quaternion = new Quaternion(new Vector3f(vector3d1), simulated, true);
-	            Vector3d vector3d = shooter.getViewVector(1.0F);
-	            Vector3f vector3f = new Vector3f(vector3d);
-	            vector3f.transform(quaternion);
-	            projectileentity.shoot((double)vector3f.x(), (double)vector3f.y(), (double)vector3f.z(), speed * this.getProjectileSpeedModifier(), divergence);
-	         }
+	public default void modifiedShootProjectile(World world, LivingEntity shooter, Hand handUsed, ItemStack crossbow, ItemStack projectileStack, float shootSoundPitch, boolean flagProjectileCantBePickedUp, float speed, float divergence,
+			float simulated) {
+		if (!world.isClientSide) {
+			boolean flag = projectileStack.getItem() == Items.FIREWORK_ROCKET;
+			ProjectileEntity projectileentity;
+			if (flag) {
+				projectileentity = new FireworkRocketEntity(world, projectileStack, shooter, shooter.getX(), shooter.getEyeY() - (double) 0.15F, shooter.getZ(), true);
+			} else {
+				projectileentity = CrossbowItem.getArrow(world, shooter, crossbow, projectileStack);
+				if (flagProjectileCantBePickedUp || simulated != 0.0F) {
+					((AbstractArrowEntity) projectileentity).pickup = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
+				}
+			}
 
-	         crossbow.hurtAndBreak(flag ? 3 : 1, shooter, (shooterTmp) -> {
-	            shooterTmp.broadcastBreakEvent(handUsed);
-	         });
-	         world.addFreshEntity(projectileentity);
-	         world.playSound((PlayerEntity)null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0F, shootSoundPitch);
-	      }
+			if (shooter instanceof ICrossbowUser) {
+				ICrossbowUser icrossbowuser = (ICrossbowUser) shooter;
+				icrossbowuser.shootCrossbowProjectile(icrossbowuser.getTarget(), crossbow, projectileentity, simulated);
+			} else {
+				Vector3d vector3d1 = shooter.getUpVector(1.0F);
+				Quaternion quaternion = new Quaternion(new Vector3f(vector3d1), simulated, true);
+				Vector3d vector3d = shooter.getViewVector(1.0F);
+				Vector3f vector3f = new Vector3f(vector3d);
+				vector3f.transform(quaternion);
+				projectileentity.shoot((double) vector3f.x(), (double) vector3f.y(), (double) vector3f.z(), speed * this.getProjectileSpeedModifier(), divergence);
+			}
+
+			crossbow.hurtAndBreak(flag ? 3 : 1, shooter, (shooterTmp) -> {
+				shooterTmp.broadcastBreakEvent(handUsed);
+			});
+			world.addFreshEntity(projectileentity);
+			world.playSound((PlayerEntity) null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0F, shootSoundPitch);
+		}
 	}
 
 	public default int modifiedGetChargeDuration(ItemStack crossbow) {
-		 int i = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.QUICK_CHARGE, crossbow);
-	     return i == 0 ? this.getMaxChargeTime() : this.getMaxChargeTime() - this.getChargeTimeReductionPerQuickChargeLevel() * i;
+		int i = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.QUICK_CHARGE, crossbow);
+		return i == 0 ? this.getMaxChargeTime() : this.getMaxChargeTime() - this.getChargeTimeReductionPerQuickChargeLevel() * i;
 	}
-	
+
 	default int getMaxChargeTime() {
 		return 25;
 	}
-	
+
 	default int getChargeTimeReductionPerQuickChargeLevel() {
 		return 5;
 	}
-	
+
 	default float getProjectileSpeedModifier() {
 		return 1.0F;
 	}
