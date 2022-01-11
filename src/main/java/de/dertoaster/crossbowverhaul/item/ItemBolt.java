d911bc84c99b8775a193a802862c8e960e7968d8
DerToaster98 <seiler.oli@web.de>
2022-01-11 17:04:41 +0100

    add base class for all bolts
    (cherry picked from commit b65c4b2a6c2ce2b8b517dccab97a4222dbe83d07)
    
    # Conflicts:
    #	src/main/java/de/dertoaster/crossbowverhaul/item/ItemBolt.java

--- src/main/java/de/dertoaster/crossbowverhaul/item/ItemBolt.java
+++ src/main/java/de/dertoaster/crossbowverhaul/item/ItemBolt.java
@@ -1,36 +1,17 @@
 package de.dertoaster.crossbowverhaul.item;
 
-import java.util.List;
-
-import org.lwjgl.glfw.GLFW;
-
 import de.dertoaster.crossbowverhaul.entity.projectile.ProjectileBolt;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.AbstractArrowEntity;
-import net.minecraft.item.ArrowItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemTier;
-import net.minecraft.world.World;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.Tiers;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
-public class ItemBolt extends ArrowItem {
-
-	protected final Tier tier;
+public class ItemBolt extends AbstractItemBolt {
 
 	public ItemBolt(Properties properties, final Tier tier) {
-		super(properties);
-		this.tier = tier;
+		super(properties, tier);
 	}
 
 	@Override
@@ -40,28 +21,4 @@ public class ItemBolt extends ArrowItem {
 		return bolt;
 	}
 
-	public int getIdForItemProperties() {
-		for(Tiers etier : Tiers.values()) {
-			if((Tier)etier == this.getTier()) {
-				return etier.ordinal();
-			}
-		}
-		return Tiers.values().length;
-	}
-	
-	public Tier getTier() {
-		return this.tier;
-	}
-	
-	@Override
-	public void appendHoverText(ItemStack p_77624_1_, Level p_77624_2_, List<Component> tooltip, TooltipFlag p_77624_4_) {
-		super.appendHoverText(p_77624_1_, p_77624_2_, tooltip, p_77624_4_);
-		if (GLFW.glfwGetKey(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT) != GLFW.GLFW_PRESS) {
-			tooltip.add(new TranslatableComponent("item." + this.getRegistryName().getNamespace() + ".tooltip.hold_shift"));
-		} else {
-			tooltip.add(new TranslatableComponent("item." + this.getRegistryName().getNamespace() + ".boltdamagetooltip", ProjectileBolt.getAdditionalDamageOf(this.getTier())));
-			tooltip.add(new TranslatableComponent("item." + this.getRegistryName().getNamespace() + "." + this.getRegistryName().getPath() + ".tooltip"));
-		}
-	}
-	
 }
