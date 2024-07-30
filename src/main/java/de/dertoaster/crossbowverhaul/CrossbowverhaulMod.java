b807a0c296d9ce4c6d01f56807a4c87aa3bb4292
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-07-30 18:13:06 +0200

    update mod main class partially

--- src/main/java/de/dertoaster/crossbowverhaul/CrossbowverhaulMod.java
+++ src/main/java/de/dertoaster/crossbowverhaul/CrossbowverhaulMod.java
@@ -1,5 +1,9 @@
 package de.dertoaster.crossbowverhaul;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,7 +25,7 @@ import net.minecraftforge.fml.loading.FMLPaths;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CrossbowverhaulMod.MODID)
-@EventBusSubscriber(modid = CrossbowverhaulMod.MODID, bus = Bus.MOD)
+@EventBusSubscriber(modid = CrossbowverhaulMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class CrossbowverhaulMod
 {
 	public static final String MODID = "crossbowverhaul";
@@ -29,7 +33,7 @@ public class CrossbowverhaulMod
     public static final Logger LOGGER = LogManager.getLogger();
 
     public CrossbowverhaulMod() {
-    	IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+    	IEventBus modbus = ModLoadingContext.get().getModEventBus();
     	
     	//Register config
     	ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, COConfig.CONFIG_SPEC, "co-config.toml");
