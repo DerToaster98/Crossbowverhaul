b14cb5bbf33d2eeca686afe6a1028570e2f30fdd
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-07-30 18:12:01 +0200

    update utility thing

--- src/main/java/de/dertoaster/crossbowverhaul/item/IRegistryNameProvider.java
+++ src/main/java/de/dertoaster/crossbowverhaul/item/IRegistryNameProvider.java
@@ -1,15 +1,15 @@
 package de.dertoaster.crossbowverhaul.item;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.common.extensions.IForgeItem;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.neoforged.neoforge.common.extensions.IItemExtension;
 
-public interface IRegistryNameProvider extends IForgeItem {
+public interface IRegistryNameProvider extends IItemExtension {
 	
 	public default ResourceLocation getRegistryName() {
 		if(this instanceof Item self) {
-			return ForgeRegistries.ITEMS.getKey(self);
+			return BuiltInRegistries.ITEM.getKey(self);
 		}
 		return null;
 	}
