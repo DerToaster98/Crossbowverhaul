ef8ffb4c95cef2440701e7500df154fa7bb30c07
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-07-30 18:11:51 +0200

    update init files

--- src/main/java/de/dertoaster/crossbowverhaul/init/ModItems.java
+++ src/main/java/de/dertoaster/crossbowverhaul/init/ModItems.java
@@ -6,37 +6,42 @@ import de.dertoaster.crossbowverhaul.item.ItemBolt;
 import de.dertoaster.crossbowverhaul.item.ItemBoltExplosive;
 import de.dertoaster.crossbowverhaul.item.ItemCrossbow;
 import de.dertoaster.crossbowverhaul.item.ItemCrossbowNetherite;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
-import net.minecraft.world.item.Tiers;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.function.Supplier;
 
 public class ModItems {
-	
-	public static void registerToEventBus(IEventBus eventbus) {
-		ITEMS.register(eventbus);
-		VANILLA_ITEMS.register(eventbus);
-	}
 
-	static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CrossbowverhaulMod.MODID);
-	static final DeferredRegister<Item> VANILLA_ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "minecraft");
-	
+    public static void registerToEventBus(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+        VANILLA_ITEMS.register(eventbus);
+    }
 
-	public static final RegistryObject<Item> ITEM_BOLT_IRON = ITEMS.register("bolt_iron", () -> new ItemBolt((new Properties().stacksTo(64)), Tiers.IRON));
-	public static final RegistryObject<Item> ITEM_BOLT_GOLD = ITEMS.register("bolt_gold", () -> new ItemBolt((new Properties().stacksTo(64)), Tiers.GOLD));
-	public static final RegistryObject<Item> ITEM_BOLT_DIAMOND = ITEMS.register("bolt_diamond", () -> new ItemBolt((new Properties().stacksTo(64)), Tiers.DIAMOND));
-	public static final RegistryObject<Item> ITEM_BOLT_NETHERITE = ITEMS.register("bolt_netherite", () -> new ItemBolt((new Properties().stacksTo(64).fireResistant()), Tiers.NETHERITE));
-	
-	//Explosive bolt
-	public static final RegistryObject<Item> ITEM_BOLT_EXPLOSIVE = ITEMS.register("bolt_explosive", () -> new ItemBoltExplosive(new Properties().stacksTo(64).fireResistant()));
+    static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CrossbowverhaulMod.MODID);
+    static final DeferredRegister.Items VANILLA_ITEMS = DeferredRegister.createItems("minecraft");
 
-	//Netherite crossbow
-	public static final RegistryObject<Item> ITEM_CROSSBOW_NETHERITE = ITEMS.register("crossbow_netherite", () -> (new ItemCrossbowNetherite(new Item.Properties().stacksTo(1).durability(COConfig.CONFIG.coNetheriteCrossbowDurability.get()).fireResistant())));
-	
-	//now, override the crossbow
-	public static final RegistryObject<Item> ITEM_CROSSBOW = VANILLA_ITEMS.register("crossbow", () -> (new ItemCrossbow(new Item.Properties().stacksTo(1).durability(COConfig.CONFIG.coCrossbowDurability.get()))));
+
+    public static final DeferredItem<Item> ITEM_BOLT_IRON = ITEMS.register("bolt_iron", () -> {
+        return new ItemBolt(new Item.Properties().stacksTo(64), Tiers.IRON);
+    });
+    public static final DeferredItem<Item> ITEM_BOLT_GOLD = ITEMS.register("bolt_gold", () -> new ItemBolt(new Item.Properties().stacksTo(64), Tiers.GOLD));
+    public static final DeferredItem<Item> ITEM_BOLT_DIAMOND = ITEMS.register("bolt_diamond", () -> new ItemBolt(new Item.Properties().stacksTo(64), Tiers.DIAMOND));
+    public static final DeferredItem<Item> ITEM_BOLT_NETHERITE = ITEMS.register("bolt_netherite", () -> new ItemBolt(new Item.Properties().stacksTo(64).fireResistant(), Tiers.NETHERITE));
+
+    //Explosive bolt
+    public static final DeferredItem<Item> ITEM_BOLT_EXPLOSIVE = ITEMS.register("bolt_explosive", () -> new ItemBoltExplosive(new Item.Properties().stacksTo(64).fireResistant()));
+
+    //Netherite crossbow
+    public static final DeferredItem<Item> ITEM_CROSSBOW_NETHERITE = ITEMS.register("crossbow_netherite", () -> (new ItemCrossbowNetherite(new Item.Properties().stacksTo(1).durability(COConfig.CONFIG.coNetheriteCrossbowDurability.get()).fireResistant())));
+
+    //now, override the crossbow
+    public static final DeferredItem<Item> ITEM_CROSSBOW = VANILLA_ITEMS.register("crossbow", () -> (new ItemCrossbow(new Item.Properties().stacksTo(1).durability(COConfig.CONFIG.coCrossbowDurability.get()))));
 
 }
